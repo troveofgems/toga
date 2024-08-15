1e8cf81346e58ba69c8a57e21e752f69757c42c0
Dustin K Greco <dkgreco@thetroveofgems.tech>
2024-08-15 09:50:48 -0700

    Commit work and move to server for testing

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,13 @@
-const path = require('path');
+const
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  path = require('path');
 
 module.exports = {
   entry: {
     app: './js/app.js',
   },
   output: {
+    publicPath: path.join(__dirname, 'dist', 'assets'),
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: './js/app.js',
